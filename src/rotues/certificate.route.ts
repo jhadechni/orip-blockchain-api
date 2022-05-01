2042add7993a3bff84ecc5de43d195553a355091
fabrixiop26 <fabriciopuche@hotmail.com>
2022-05-01 16:49:40 -0500

    config ipfs and cipher

--- src/rotues/certificate.route.ts
+++ src/rotues/certificate.route.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
-import { ipfsUpload } from "@tatumio/tatum";
+import { NFTStorage as Storage, Blob } from "nft.storage";
+import { configService } from "../config/config.service";
 const route = Router();
 
-route.post("/create", async (req, res) => {
-  // await ipfsUpload()
+route.get("/create", async (req, res) => {
+  try {
+    const blob = new Blob([JSON.stringify({ message: "Jaime es muy Gay" })]);
+    // create a new NFTStorage client using our API key
+    const ipfsStorage = new Storage({
+      token: configService.get("STORAGE_KEY"),
+    });
+
+    const result = await ipfsStorage.storeBlob(blob);
+
+    res.status(200).json({ ipfsHash: result });
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+    console.error(e);
+  }
 });
 
 export default route;
--- src/rotues/index.ts
+++ src/rotues/index.ts
@@ -1,8 +1,10 @@
 import { Express } from "express";
 import UserRoute from "./user.route";
+import CertificateRoute from "./certificate.route";
 
 const configRoutes = (app: Express) => {
   app.use("/users", UserRoute);
+  app.use("/certificate", CertificateRoute);
 };
 
 export default configRoutes;
